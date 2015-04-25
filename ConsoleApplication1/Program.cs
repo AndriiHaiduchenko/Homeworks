709d43824d71bb8066f829381bcf6b49cdcd1249
Andrii <demonictoast@gmail.com>
2015-04-25 14:52:15 +0300

    Homework

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+        // This calc is using lambda expressions;
+
+namespace ConsoleApplication1
+{
+    public delegate double CalcDelegate(int a, int b);
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter first number:");
+            int a = int.Parse(Console.ReadLine());          // .Parse or Convert.ToInt32? 
+            Console.WriteLine("Enter second number:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operator:");
+            string operate = Console.ReadLine();
+
+            CalcDelegate myOperator = null;
+
+            switch (operate)
+            {
+                case "+":
+                    myOperator = (x, y) => { return x + y; };
+                    break;
+
+                case "-":
+                    myOperator = (x, y) => { return x - y; };
+                    break;
+
+                case "*":
+                    myOperator = (x, y) => { return x * y; };
+                    break;
+
+                case "/":
+                    myOperator = (x, y) =>
+                    {
+                        if (y != 0)                 // zero check;
+                        {
+                            return x / (double)y;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cannot divide by zero!");
+                            return 0;               // Delegate returns double. 
+                        }
+                    };
+                    break;
+
+                default:
+                    Console.WriteLine("This operation is not supported");
+                    break;
+            }
+            if (myOperator != null)
+                Console.WriteLine(myOperator(a, b));
+        }
+    }
+}
---
+++ Homeworks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// Anonymous method 
+
+namespace Homeworks
+{
+    class Program
+    {
+
+        private delegate double MeanDelegate(int a, int b, int c);
+
+        static void Main(string[] args)
+        {
+            int d = 3;
+            int e = 4;
+            int f = 10;
+
+            MeanDelegate mydelegate = (a, b, c) => (double) (a + b + c)/3;//Arithmetic mean of three numbers;
+            Console.WriteLine(mydelegate(d,e,f));
+        }
+    }
+}
